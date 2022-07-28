22e5acf49832540083c4ee6503251ff54786482f
Thanh Trinh <thanchitrinh2000@gmail.com>
2022-07-28 11:42:51 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/home', to: 'home_pages#home'
   get '/help', to: 'home_pages#help'
   get '/about', to: 'home_pages#about'
-  get '/signup', to: 'users#new'
+  resources :users, only: [:new, :show, :create], path_names: {new: 'signup'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
