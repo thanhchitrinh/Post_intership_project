0b6665bc2b883e5a9e2147f17a9b85e3573a322c
Thanh Trinh <thanchitrinh2000@gmail.com>
2022-07-28 20:40:31 +0700

    Hard_Problem

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   get '/home', to: 'home_pages#home'
   get '/help', to: 'home_pages#help'
   get '/about', to: 'home_pages#about'
-  resources :users, only: [:new, :show, :create], path_names: {new: 'signup'}
+  resources :users, only: [:new, :show, :create, :index], path_names: {new: 'signup'}
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts, only: [:index, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
